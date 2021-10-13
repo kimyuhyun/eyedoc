3a66317624ed5a4325f8a4d0da3a429f5a907d74
kimyuhyun <escnet@naver.com>
2021-10-13 10:27:10 +0900

    2

--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,7 @@ router.get('/', function(req, res, next) {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     res.render('index', {
-        title: 'Eyedoc Api',
+        title: 'eyedoc api',
         session: `${ip}`,
         mode: process.env.NODE_ENV,
     });
