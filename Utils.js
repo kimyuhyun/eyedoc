6618171af9a87bb16850c09a3cd3398cf3eccd19
kimyuhyun <escnet@naver.com>
2021-10-20 10:57:09 +0900

    last

--- Utils.js
+++ Utils.js
@@ -231,30 +231,14 @@ class Utils {
     getAge(birth) {
         var date = new Date();
         var year = date.getFullYear();
-        var month = (date.getMonth() + 1);
-        var day = date.getDate();
-        if (month < 10) month = '0' + month;
-        if (day < 10) day = '0' + day;
-        var monthDay = month + day;
-        birth = birth.replace('-', '').replace('-', '');
-        var birthdayy = birth.substr(0, 4);
-        var birthdaymd = birth.substr(4, 4);
-        var age = monthDay < birthdaymd ? year - birthdayy - 1 : year - birthdayy;
+        var tmp = birth.split('-')[0];
+        var age = year - tmp;
         return age;
     }
 
     getAge2(birth, year) {
-        var date = new Date(year);
-        var year = date.getFullYear();
-        var month = (date.getMonth() + 1);
-        var day = date.getDate();
-        if (month < 10) month = '0' + month;
-        if (day < 10) day = '0' + day;
-        var monthDay = month + day;
-        birth = birth.replace('-', '').replace('-', '');
-        var birthdayy = birth.substr(0, 4);
-        var birthdaymd = birth.substr(4, 4);
-        var age = monthDay < birthdaymd ? year - birthdayy - 1 : year - birthdayy;
+        var tmp = birth.split('-')[0];
+        var age = year - tmp;
         return age;
     }
 
